c89d7e50c349c6d474d1035f1e3c842ab223f436
wanghao1891 <wanghao1891@126.com>
2016-02-04 08:46:04 +0800

    Separate signin view from index.ios.

---
+++ foodmobile/signin.js
@@ -0,0 +1,106 @@
+/**
+ * Singin View
+ */
+
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight
+} from 'react-native';
+
+function render() {
+  return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Food
+        </Text>
+        <Text style={styles.description}>
+          Record the detail of food
+        </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+             style={styles.signInput}
+             onChangeText={(username) => this.setState({username})}
+            value={this.props.username}
+             placeholder='Name'/>
+          <TextInput
+             style={styles.signInput}
+             onChange={this.on_password_change}
+             value={this.props.password}
+             secureTextEntry={true}
+             placeholder='Password'/>
+        </View>
+        <TouchableHighlight style={styles.button}
+                            onPress={this.signin}
+                            underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button}
+                            onPress={this.signin}
+                            underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </TouchableHighlight>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  },
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  flowRight: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  signInput: {
+    //width: 200,
+    height: 36,
+    padding: 4,
+    marginBottom: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 5,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  }
+});
+
+var options = {
+  render: render
+};
+
+var SigninView = React.createClass(options);
+
+module.exports = SigninView;
